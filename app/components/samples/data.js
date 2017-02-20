d76ee6e5e6aba33502a1d06bfb3093b072a04167
madsam753 <sam92.sp@gmail.com>
2017-02-20 09:55:54 +0100

    Changed url links to use dropbox. Favicon added.

--- app/components/samples/data.js
+++ app/components/samples/data.js
@@ -1,4 +1,5 @@
 
+/*
 let samples = [
   'http://freewavesamples.com/files/Kick-Drum-16.wav',
   'http://freewavesamples.com/files/Dry-Kick.wav',
@@ -30,6 +31,45 @@ let samples = [
   'http://freewavesamples.com/files/1980s-Casio-Piano-C5.wav',
   'http://freewavesamples.com/files/Alesis-Fusion-Acoustic-Bass-C2.wav',
   'http://freewavesamples.com/files/Electro-Tom.wav',
+];
+*/
+
+let samples = [
+  // Kicks
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-16.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Bass-Drum-1.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Bass-Drum-2.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Bass-Drum-3.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Dry-Kick.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/E-Mu-Proteus-FX-909-Kick.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Ensoniq-SQ-1-Rock-Kick.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kawai-K1r-Rap-Kick.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kawai-K11-Bob-Kick.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-11.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-12.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-13.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-16.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-17.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/kick/Kick-Drum-18.wav',
+
+  // Snares
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/Korg-N1R-Snare-Drum-2.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/E-Mu-Proteus-FX-Wacky-Snare.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/Ensoniq-ESQ-1-Snare.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/Ensoniq-SQ-1-Rock-Snare.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/Hip-Hop-Snare-1.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/snare/Korg-M3R-Snare.wav',
+
+  // Hihats
+  'https://dl.dropboxusercontent.com/u/76439474/samples/hihat/Closed-Hi-Hat-1.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/hihat/Closed-Hi-Hat-2.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/hihat/Closed-Hi-Hat-3.wav',
+
+  // Others
+  'https://dl.dropboxusercontent.com/u/76439474/samples/others/1980s-Casio-Piano-C5.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/others/Alesis-Fusion-Acoustic-Bass-C2.wav',
+  'https://dl.dropboxusercontent.com/u/76439474/samples/others/Electro-Tom.wav',
+
 ];
 
 export default samples;
